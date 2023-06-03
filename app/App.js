6f7769b293b34871a91fb674984c6eaf357b2cf0
Zapilman <17uraz12@gmail.com>
2023-06-03 10:22:09 +0300

    app init

---
+++ app/App.js
@@ -0,0 +1,11 @@
+import LoginPage from '../core/pages/LoginPage/LoginPage';
+
+const App = () => {
+  return (
+    <div>
+      <div>some shit</div>
+      <LoginPage />
+    </div>
+  );
+};
+export default App;
---
+++ app/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  entry: './app/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+  resolve: {
+    modules: [__dirname, 'app', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts', '...'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: require.resolve('babel-loader'),
+      },
+      {
+        test: /\.s[ac]ss|css$/i,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+            },
+          },
+          { loader: 'sass-loader' },
+        ],
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.html',
+    }),
+    // new BundleAnalyzerPlugin(),
+  ],
+};
